154d7dbbdef8fdae6068a3cf4f371e573df9e8b4
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-08-27 07:42:32 +0700

    usermaifam.py

--- usermaifam.py
+++ usermaifam.py
@@ -140,46 +140,46 @@ async def handler_memancing(event):
     global respond_to_group
     if not respond_to_group:
       
-    pesan = event.raw_text
-
-    for key, values in pesan_dict.items():
-        for value in values:
-            if value in pesan:
-                klik = await client.get_messages(mepamx, ids=event.message.id)
-                time.sleep(tunggu)
-                await klik.click(text=alat)
-                break 
-  
-            if "Kamu berhasil" in pesan or "Kamu mendapat" in pesan:
-                await asyncio.sleep(2)
-                if area in areas_dict:
-                    await event.respond(areas_dict[area])
-                print(pesan)
-                
+        pesan = event.raw_text
+    
+        for key, values in pesan_dict.items():
+            for value in values:
+                if value in pesan:
+                    klik = await client.get_messages(mepamx, ids=event.message.id)
+                    time.sleep(tunggu)
+                    await klik.click(text=alat)
+                    break 
+      
+                if "Kamu berhasil" in pesan or "Kamu mendapat" in pesan:
+                    await asyncio.sleep(2)
+                    if area in areas_dict:
+                        await event.respond(areas_dict[area])
+                    print(pesan)
                     
-            elif 'tidak memiliki cukup energi' in pesan:
-                await asyncio.sleep(2)
-                await event.respond('/restore_max_confirm')
-                print(time.asctime(), 'Isi Ulang Energi')
-                    
-                            
-            elif 'Energi berhasil' in pesan:
-                await asyncio.sleep(2)
-                await event.click(text=alat)
-                print('Lanjut Gan')
-                    
-                          
-            elif '==' in pesan:
-                await asyncio.sleep(2)
-                await event.click(text=alat)
-                    
-                            
-            elif 'Kamu tidak sedang' in pesan:
-                await asyncio.sleep(2)
-                if area in areas_dict:
-                    await event.respond(areas_dict[area])
-                print(pesan)
-        
+                        
+                elif 'tidak memiliki cukup energi' in pesan:
+                    await asyncio.sleep(2)
+                    await event.respond('/restore_max_confirm')
+                    print(time.asctime(), 'Isi Ulang Energi')
+                        
+                                
+                elif 'Energi berhasil' in pesan:
+                    await asyncio.sleep(2)
+                    await event.click(text=alat)
+                    print('Lanjut Gan')
+                        
+                              
+                elif '==' in pesan:
+                    await asyncio.sleep(2)
+                    await event.click(text=alat)
+                        
+                                
+                elif 'Kamu tidak sedang' in pesan:
+                    await asyncio.sleep(2)
+                    if area in areas_dict:
+                        await event.respond(areas_dict[area])
+                    print(pesan)
+            
         
         
 @client.on(events.NewMessage(from_users=mepamx4))
