ccb7c68f77d67994ed22b15036dfd64a723b9176
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-09-02 23:23:39 +0700

    Add files via upload

---
+++ horangfarm.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import time, asyncio, sys, random
+
+
+from telethon import TelegramClient, events, utils
+
+api_id = 18850178 
+api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
+sesi_file = 'Horang'
+
+siram = '/siram'
+panen = '/ambilPanen'
+bot = ['danaudalamhutan', 'KampungMaifamXBot', 'KampungMaifamX4Bot', 'KampungMaifamBot']
+    
+async def bentar(w):
+    await asyncio.sleep(w)
+
+async def mancingddh(client,w):
+    while True:
+        await client.send_message(bot[0], "/fish")
+        await bentar(w)
+        
+with TelegramClient(sesi_file, api_id, api_hash) as client:
+    client.loop.run_until_complete(client.send_message(bot[1], siram))
+
+    @client.on(events.NewMessage(from_users=bot[1]))
+    async def handler(event):
+        pesan = event.raw_text
+        
+        
+        if "Berhasil menyiram tanaman" in pesan:
+            time.sleep(1265)
+            await event.respond(panen)
+            return
+            
+        if "Kamu berhasil memanen" in pesan:
+            print(time.asctime(), pesan)
+            time.sleep(2)
+            await event.respond(siram)
+            return
+        
+            
+        if "Tak ada yang bisa dipanen" in pesan:
+            time.sleep(2)
+            await event.respond(siram)
+            return
+            
+        if "Lahan tersisa di kebun kamu tidak mencukupi" in pesan:
+            time.sleep(2)
+            await event.respond(panen)
+            return
+         
+        if 'Kamu tidak memiliki cukup energi' in pesan:
+            print('Energi habis')
+            time.sleep(2)
+            await event.respond('/restore_max_confirm')
+            return
+            
+        if 'Energi berhasil dipulihkan' in pesan:
+            time.sleep(2)
+            await event.respond(siram)
+            return
+        
+        if 'Tak ada tanaman untuk disiram' in pesan:
+            print(pesan)
+            time.sleep(1265)
+            await event.respond(panen)
+            return
+          
+       
+        
+    client.start() 
+    print(time.asctime(), '-', 'start')
+    client.loop.create_task(mancingddh(client,245))
+    client.run_until_disconnected() 
+    print(time.asctime(), '-', 'berhenti')
