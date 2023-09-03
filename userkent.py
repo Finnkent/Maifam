31f25dae9793ad7ff7a5b6641f7d1ab8519c4a9f
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-09-03 07:25:17 +0700

    userkent.py

--- userkent.py
+++ userkent.py
@@ -355,7 +355,7 @@ async def handler_one(event):
         await event.respond('/restore')  
                 
                 
-    if "anggota kelompokmu" in pesan:
+    if "anggota kelompokmu" in pesan or "kelompok musuh kabur" in pesan:
         time.sleep(2)
         await event.click(0,0)
                 
@@ -365,7 +365,7 @@ async def handler_one(event):
         await event.click(0,0)
                 
               
-    if "untuk bisa lanjut ke pulau" in pesan:
+    if "untuk bisa lanjut ke pulau" in pesan pr "Tidak ada lawan yang bisa dilawan" in pesan:
         time.sleep(2)
         await event.click(0,0)
         
