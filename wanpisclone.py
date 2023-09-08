34872ec469572611f67c580a96a2667246a76490
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-09-08 11:19:43 +0700

    wanpisclone.py

--- wanpisclone.py
+++ wanpisclone.py
@@ -1,111 +1,184 @@
-import time, asyncio, sys, random
-import logging
-from telethon import TelegramClient, events, utils, Button
+import time
+import asyncio
 
-api_id = 18850178 
+from telethon.sync import TelegramClient
+from telethon import events
+
+from tqdm import tqdm
+from rich.progress import Progress
+
+api_id = 18850178
 api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
-sesi_file = input("Akun : ")
+
+punyaku = [
+  'Keisya',
+  'Mio',
+  'Jenia',
+  'Cibu', 
+  'Eunji', 
+  'Fainry', 
+  'Blu', 
+  'Genya', 
+  'Minu',
+  'Einly',
+  
+]
 
 bot_id = 'GrandPiratesBot'
 adv = '/adventure'
-
-logging.basicConfig(level=logging.ERROR)
-
-def tunggu_hingga_menit_detik_00():
-    saat_ini = time.localtime()
-    menit_sekarang = saat_ini.tm_min
-    detik_sekarang = saat_ini.tm_sec
-    
-    if menit_sekarang == 0 and detik_sekarang == 0:
-        return  # Sudah 00:00, tidak perlu menunggu
-    
-    detik_yang_harus_ditunggu = (60 - menit_sekarang) * 60 - detik_sekarang
-    print(f"Menunggu hingga 00:00.")
-    time.sleep(detik_yang_harus_ditunggu)
+kill = 0
+ex = '/use_EXPPill'
 
 
-with TelegramClient(sesi_file, api_id, api_hash) as client:
-        client.loop.run_until_complete(client.send_message(bot_id, adv))
-        @client.on(events.NewMessage(from_users=bot_id))
-        async def handler(event):
-            pesan = event.raw_text
-            
-            if "sudah mencapai batas maksimal" in pesan:
-                tunggu_hingga_menit_detik_00()
-                await event.respond('/adventure')
-                return
-            
-            
-            elif "Kalahkan musuh yang ada" in pesan or "Kalahkan semua musuh" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
-              
-            elif "dan dihadang oleh 4 musuh:" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
-            
-            elif "dan dihadang oleh 3 musuh:" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
-            
-            elif "dan dihadang oleh 2 musuh:" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
+async def my_function():
+    while True:
+        for aaa in punyaku:
+            async with TelegramClient(aaa, api_id, api_hash) as client:
+                await client.send_message(bot_id, adv)
                 
-            elif "dan dihadang oleh 1 musuh:" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
-                
-            elif "KAMU MENANG!!" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
-              
-            elif "Musuh menang" in pesan:
-                if "untuk mencapai kekuatan" in pesan:
-                    time.sleep(2)
-                    await event.respond('/restore')
-                else:
-                    time.sleep(2)
-                    await event.click(0,0)
-                return
-            
-            elif "Energi krumu telah habis" in pesan:
-                time.sleep(2)
-                await event.respond('/restore')  
-                return
-                
-            elif "anggota kelompokmu" in pesan or "musuh kabur" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
-              
-            elif "100x!! Kamu mendapat" in pesan or "250x!! Sekarang ia" in pesan:
-                time.sleep(2)
-                await event.respond(adv)
-                return
-              
-            elif "untuk bisa lanjut ke pulau" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
-                
-            elif "Berhasil memulihkan" in pesan:
-                time.sleep(2)
-                await event.respond(adv)
-                return
-                
-            elif "kekuatan kalian sebagai" in pesan:
-                time.sleep(2)
-                await event.click(0,0)
-                return
-            
-            
-client.start()
-print('Started')
-client.run_until_disconnected()
+                @client.on(events.NewMessage(from_users=adv))
+                async def handler(event):
+                    pesan = event.text
+                      
+                    if "100x!! Kamu mendapat" in pesan or "250x!! Sekarang ia" in pesan or "250x!! Kamu mendapat" in pesan:
+                        time.sleep(2)
+                        await event.respond(adv)
+                        return
+                      
+                    if "Energi krumu telah habis" in pesan or "musuh kabur" in pesan:
+                        time.sleep(2)
+                        await event.respond('/restore')  
+                        return
+                    
+                    if kill >= 10:
+                        time.sleep(2)
+                        await event.respond(ex)  
+                        kill = 0
+                        return
+                    
+                    
+                    elif "Kalahkan musuh yang ada" in pesan or "Kalahkan semua musuh" in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                      
+                    elif "dan dihadang oleh 4 musuh:" in pesan:
+                        time.sleep(2)
+                        await event.click(1,0)
+                        return
+                    
+                    elif "dan dihadang oleh 3 musuh:" in pesan:
+                        time.sleep(2)
+                        await event.click(1,0)
+                        return
+                    
+                    elif "dan dihadang oleh 2 musuh:" in pesan:
+                        time.sleep(2)
+                        await event.click(1,0)
+                        return
+                        
+                    elif "dan dihadang oleh 1 musuh:" in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                     
+                     
+                        
+                    elif "KAMU MENANG!!" in pesan:
+                        kill += 1
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                      
+                    elif "Musuh menang" in pesan:
+                        if "untuk mencapai kekuatan" in pesan:
+                            time.sleep(2)
+                            await event.respond('/restore')
+                        else:
+                            time.sleep(2)
+                            await event.click(0,0)
+                        return
+                    
+                        
+                    elif "sedang tidak dalam kondisi" in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                      
+                      
+                    elif "untuk bisa lanjut ke pulau" in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                        
+                    elif "Berhasil memulihkan" in pesan:
+                        time.sleep(2)
+                        await event.respond(ex)
+                        return
+                      
+                    elif "Berhasil menggunakan 💊EXPPill" in pesan:
+                        time.sleep(2)
+                        await event.respond(ex)
+                        return
+                      
+                    elif "maksimal 10 item buff" in pesan or "Kamu tidak memiliki 💊EXPPill" in pesan:
+                        time.sleep(2)
+                        await event.respond(adv)
+                        return
+                      
+                    
+                        
+                    elif "kekuatan kalian sebagai" in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                    
+                    #EastBlue: ShellsTown
+                    elif 'Kota pinggir laut' in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                    
+                    #EastBlue: OrangeTown
+                    elif 'Kota kecil yang ditumbuhi' in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                    
+                    #EastBlue: SyrupVillage
+                    elif 'Desa sederhana yang indah' in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                    
+                    #EastBlue: Baratie
+                    elif 'Sebuah restauran yang mengapung' in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                    
+                    #EastBlue: ArlongPark
+                    elif 'Kastil manusia ikan' in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                      
+                    
+                    #EastBlue: Loguetown
+                    elif 'Dikenal sebagai kota awal mula' in pesan:
+                        time.sleep(2)
+                        await event.click(0,0)
+                        return
+                      
+                    if "maksimal 100x tiap jamnya" in pesan or "sedang dalam perjalanan menuju" in pesan:
+                        await asyncio.sleep(1)
+                        await client.disconnect()
+                        close()
+
+                await client.run_until_disconnected()
+            print(aaa, '- Complete -', time.asctime())
+        for remaining in tqdm(range(120), desc="Waiting"):
+            await asyncio.sleep(1)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(my_function())
