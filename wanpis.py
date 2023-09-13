40233051733db631e07747ded3a0a851478750dd
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-09-13 19:20:29 +0700

    wanpis.py

--- wanpis.py
+++ wanpis.py
@@ -69,17 +69,17 @@ with TelegramClient(sesi_file, api_id, api_hash) as client:
             
             elif "dan dihadang oleh 3 musuh:" in pesan:
                 time.sleep(2)
-                await event.click(0,0)
+                await event.click(1,0)
                 return
             
             elif "dan dihadang oleh 2 musuh:" in pesan:
                 time.sleep(2)
-                await event.click(0,0)
+                await event.click(1,0)
                 return
                 
             elif "dan dihadang oleh 1 musuh:" in pesan:
                 time.sleep(2)
-                await event.click(0,0)
+                await event.click(1,0)
                 return
              
              
