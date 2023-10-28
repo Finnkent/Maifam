52f1e6d2386630c234c1845619a12c15deda1f0e
Frizi Leo Pratama <134615961+Finnkent@users.noreply.github.com>
2023-10-28 19:48:35 +0700

    halloween.py

--- halloween.py
+++ halloween.py
@@ -6,8 +6,7 @@ from telethon import TelegramClient, events, utils, Button
 
 api_id = 18850178 
 api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
-sesi_file = "Finnkent"
-
+sesi_file = input("Akun : ")
 bot_id = 'KampungMaifamXBot'
 adv = '/Halloween2023'
 
