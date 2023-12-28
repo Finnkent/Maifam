9ac3686108b5404b017960fbd7aebe7800c9f85d
Frizi Leo Pratama <134615961+Finnkent@users.noreply.github.com>
2023-12-28 18:54:49 +0700

    Add files via upload

---
+++ event.py
@@ -0,0 +1,46 @@
+import time, asyncio, sys, random
+
+import logging
+
+from telethon import TelegramClient, events, utils, Button
+
+api_id = 18850178 
+api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
+sesi_file = input("Akun : ")
+
+bot_id = 'KampungMaifamX4Bot'
+cmd = '/xm2023_GudangSanta_1'
+grup = -1001611827546
+
+logging.basicConfig(level=logging.ERROR)
+
+
+with TelegramClient(sesi_file, api_id, api_hash) as client:
+        client.loop.run_until_complete(client.send_message(bot_id, cmd))
+        @client.on(events.NewMessage(from_users=bot_id))
+        async def handler(event):
+            pesan = event.raw_text
+            
+              
+            if "namun ia telah kabur" in pesan or "Kamu mencuri dari" in pesan or "telah kabur..." in pesan:
+                time.sleep(3)
+                await event.click(text="Cari Hadiah")
+                return
+              
+            elif "dari jauh melihat" in pesan:
+                time.sleep(3)
+                await event.click(text="Curi")
+                return
+            
+            elif "menemukan sebuah" in pesan:
+                time.sleep(5)
+                await event.click(text="Ambil")
+                return
+              
+            elif "Berhasil mengambil 📱 iPong15ProMag" in pesan:
+                await client.forward_messages(grup, event.message)
+                return
+            
+client.start()
+print('Started')
+client.run_until_disconnected()
