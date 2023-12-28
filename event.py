773e239f6a55449ecf184096224d95e9a70a86c3
Frizi Leo Pratama <134615961+Finnkent@users.noreply.github.com>
2023-12-28 18:59:57 +0700

    event.py

--- event.py
+++ event.py
@@ -9,7 +9,7 @@ api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
 sesi_file = input("Akun : ")
 
 bot_id = 'KampungMaifamX4Bot'
-cmd = '/xm2023_GudangSanta_1'
+cmd = '/xm2023_GudangSanta'
 grup = -1001611827546
 
 logging.basicConfig(level=logging.ERROR)
@@ -23,17 +23,22 @@ with TelegramClient(sesi_file, api_id, api_hash) as client:
             
               
             if "namun ia telah kabur" in pesan or "Kamu mencuri dari" in pesan or "telah kabur..." in pesan:
-                time.sleep(3)
+                time.sleep(5.2)
+                await event.click(text="Cari Hadiah")
+                return
+              
+            elif "Pelan-pelan" in pesan:
+                time.sleep(5.2)
                 await event.click(text="Cari Hadiah")
                 return
               
             elif "dari jauh melihat" in pesan:
-                time.sleep(3)
+                time.sleep(4)
                 await event.click(text="Curi")
                 return
             
             elif "menemukan sebuah" in pesan:
-                time.sleep(5)
+                time.sleep(5.2)
                 await event.click(text="Ambil")
                 return
               
