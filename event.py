d383ffa26251ae9585554bde2b779c9927ce8959
Frizi Leo Pratama <134615961+Finnkent@users.noreply.github.com>
2023-12-28 19:10:59 +0700

    event.py

--- event.py
+++ event.py
@@ -1,9 +1,5 @@
 import time, asyncio, sys, random
-
-
-
 import logging
-
 from telethon import TelegramClient, events, utils, Button
 
 api_id = 18850178 
@@ -11,44 +7,54 @@ api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
 sesi_file = input("Akun : ")
 
 bot_id = 'KampungMaifamX4Bot'
-cmd = '/xm2023_GudangSanta'
 grup = -1001611827546
-jeda = 6
+gudang = '/xm2023_SantaWarehouse'
 
 logging.basicConfig(level=logging.ERROR)
-
-
+    
 with TelegramClient(sesi_file, api_id, api_hash) as client:
-        client.loop.run_until_complete(client.send_message(bot_id, cmd))
-        @client.on(events.NewMessage(from_users=bot_id))
-        async def handler(event):
-            pesan = event.raw_text
-            
-              
-            if "namun ia telah kabur" in pesan or "Kamu mencuri dari" in pesan or "telah kabur..." in pesan:
-                time.sleep(jeda)
-                await event.click(text="Cari Hadiah")
-                return
-              
-            elif "Pelan-pelan" in pesan or "Berhasil mengambil" in pesan:
-                time.sleep(jeda)
-                await event.click(text="Cari Hadiah")
-                return
-              
-            elif "dari jauh melihat" in pesan:
-                time.sleep(jeda)
-                await event.click(text="Curi")
-                return
-            
-            elif "menemukan sebuah" in pesan:
-                time.sleep(jeda)
-                await event.click(text="Ambil")
-                return
-              
-            elif "Berhasil mengambil 📱 iPong15ProMag" in pesan:
-                await client.forward_messages(grup, event.message)
-                return
-            
-client.start()
-print('Started')
-client.run_until_disconnected()
+    client.loop.run_until_complete(client.send_message('KampungMaifamX4Bot', gudang))
+
+    @client.on(events.NewMessage(from_users='KampungMaifamX4Bot'))
+    async def handler(event):
+        global gas
+
+        if "Hadiah akan direset" in event.raw_text:
+            gas = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            time.sleep(6)
+            await gas.click(text='Cari Hadiah')
+            return
+        
+        if 'ingin mencuri hadiah' in event.raw_text:
+            gas = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            time.sleep(6.2)
+            await gas.click(text='Curi')
+            return
+        
+        if "dan menemukan sebuah" in event.raw_text:
+            gas = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            time.sleep(5.2)
+            await gas.click(text='Ambil')
+            return
+        
+        if 'Berhasil mengambil' in event.raw_text:
+            gas = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            time.sleep(6.5)
+            await gas.click(text='Cari Hadiah')
+            return
+
+        if 'Pelan-pelan,' in event.raw_text:
+            gas = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            time.sleep(6)
+            await gas.click(text='Cari Hadiah')
+            return
+
+        if 'Kamu mencuri dari' in event.raw_text:
+            gas = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            time.sleep(6)
+            await gas.click(text='Cari Hadiah')
+            return
+          
+    client.start()
+    client.run_until_disconnected()
+    print(time.asctime(), '-', 'Berhenti')
